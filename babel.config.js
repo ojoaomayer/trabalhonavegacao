47980f4b6dd3676d751ec5fc0d51e8e1872caf3f
Mayer <163020798+ojoaomayer@users.noreply.github.com>
2025-11-03 08:54:59 -0300

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    // ATENÇÃO: Este plugin deve ser o último da lista de plugins.
-    'react-native-reanimated/plugin',
-  ],
-};
